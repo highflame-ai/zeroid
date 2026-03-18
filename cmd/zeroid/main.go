3a82ae6736a8c8a7a0f8f2ff973e74494b16b399
Yash Datta <yd2590@columbia.edu>
2026-03-18 12:41:57 +0800

    fix: Fix gitignore

---
+++ cmd/zeroid/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	zeroid "github.com/zeroid-dev/zeroid"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	configFile := flag.String("config", "", "Path to configuration file")
+	flag.Parse()
+
+	// Use ZEROID_CONFIG_FILE env var as fallback if no flag provided.
+	cfgPath := *configFile
+	if cfgPath == "" {
+		cfgPath = os.Getenv("ZEROID_CONFIG_FILE")
+	}
+
+	cfg, err := zeroid.LoadConfig(cfgPath)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to load configuration")
+	}
+
+	srv, err := zeroid.NewServer(cfg)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to initialize server")
+	}
+
+	if err := srv.Start(); err != nil {
+		log.Fatal().Err(err).Msg("Server error")
+	}
+}
